130ceae4fc368d42810b83dc6aa96c07950e580c
Michael Brandt <mbrandt@colorado.edu>
2014-05-11 01:43:15 -0600

    modify the 'Always Stack' module to work without overriding Goko methods, instead listening for native Goko events, as suggested by #217

--- src/ext/alwaysStack.js
+++ src/ext/alwaysStack.js
@@ -1,15 +1,46 @@
 (function () {
-    "use strict";
+    'use strict';
 
     console.log('Loading Always Stack');
 
     var mod = GS.modules.alwaysStack = new GS.Module('Always Stack');
-    mod.dependencies = ['GS', 'FS.Cards.CardStackPanel'];
+    mod.dependencies = ['GS'];
     mod.load = function () {
-        GS.alsoDo(FS.Cards.CardStackPanel, 'addView', null, function (view, index) {
-            if (GS.get_option('always_stack') && this.autoStackCards) {
-                this.stackCards = true;
+        var alwaysStack;
+
+        // first bind to gameServerHello so an instance of DominionClient is
+        // available
+        mtgRoom.conn.bind('gameServerHello', function (msg) {
+            var gameClient;
+
+            gameClient = _.find(mtgRoom.games, function (game) {
+                return game.gameAddress === msg.data.gameServerAddress;
+            }, this);
+
+            // bind to the native Goko event
+            if (GS.get_option('always_stack')) {
+                gameClient.clientConnection.bind('moveCards', alwaysStack);
             }
         });
+
+        alwaysStack = function () {
+            var dominionWindow,
+                fanPanels;
+
+            dominionWindow = this.gameInstance.playerController.dominionWindow;
+
+            // select the panels used to display cards in a fanned-out manner,
+            // i.e., the panels used to display the cards in hand
+            fanPanels = _.select(dominionWindow.panels, function (panel) {
+                return panel instanceof FS.Cards.FanCardStackPanel;
+            }, this);
+
+            // default value for numCardsAutoStackTrigger is 10; set to 0 so
+            // auto-stacking is always triggered
+            _.each(fanPanels, function (panel) {
+                panel.numCardsAutoStackTrigger = 0;
+            }, this);
+        };
+
     };
 }());
